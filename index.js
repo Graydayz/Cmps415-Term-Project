dc5985d76359a3384cfaa35dc6cdd07533965a7a
Graydayz <157071141+Graydayz@users.noreply.github.com>
2026-04-23 16:14:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static Files
-const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
